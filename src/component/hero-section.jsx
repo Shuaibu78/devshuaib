74c2be44dd27fdb5df4dab3f47d9c8b619bc7bc7
shuaibu muhammad <devshuaib@gmail.com>
2021-01-10 12:18:37 +0100

    changed profile-pic

--- src/component/hero-section.jsx
+++ src/component/hero-section.jsx
@@ -5,10 +5,10 @@ const HeroSection = () => {
   return (
     <section id="home" className="flex justify-center items-center pt-32 w-full bg-gray-hero">
       <div className="flex flex-col-reverse sm:flex-row items-center justify-between w-11/12 sm:w-10/12 mx-auto">
-        <div className=" flex items-center pb-24 justify-between flex-col w-11/12 sm:w-1/2">
+        <div className=" pt-6 sm:pt-0 flex items-center pb-24 justify-between flex-col w-11/12 sm:w-1/2">
           <div className="pb-6 sm:self-start self-center">
-            <span className="text-white text-xl md:text-2xl lg:text-3xl capitalize">hello, I'm shuaib muhammad,</span>
-            <h1 className="text-gray-400 font-bold text-2xl leading-snug md:text-3xl lg:leading-tight lg:text-4xl capitalize">a frontend engineer.</h1>
+            <span className="text-white text-1xl md:text-2xl lg:text-3xl capitalize">hello, I'm shuaib muhammad,</span>
+            <h1 className="text-gray-400 font-bold text-3xl leading-snug md:text-3xl lg:leading-tight lg:text-4xl capitalize">a frontend engineer.</h1>
           </div>
           <div className="sm:self-start self-center my-6">
             <a href="#contact" className="bg-gold-100 text-black py-4 px-10 sm:px-16 text-sm sm:text-base md:text-lg lg:text-xl xl:text-1xl font-semibold">
@@ -24,4 +24,4 @@ const HeroSection = () => {
   )
 }
 
-export default HeroSection
+export default HeroSection;
